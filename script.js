d1d1c62b43b70d08ce55034b5911fabdd20e8ecc
Saqib <Splasher733@gmail.com>
2024-05-22 13:30:16 +0500

    ..

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function RocketHead(Canvas, x, y, width, height, curveAmount, rotationAngle) {
   Canvas.translate(-(x + width / 2), -(y + height / 2));
 
   Canvas.beginPath();
-  Canvas.rect(x, y, width, height / 6); 
+  Canvas.rect(x, y, width, height / 6);
   Canvas.clip();
 
   Canvas.fillStyle = "#f64a50";
@@ -45,10 +45,9 @@ function RocketMid(Canvas, x, y, width, height, curveAmount, rotationAngle) {
 }
 
 function RocketEnd(Canvas, x, y, width, height, curveAmount, rotationAngle) {
-  
   RocketWings(Canvas, 310, 340, 20, 70, 35, Math.PI / 2.4);
   RocketWings(Canvas, 355, 400, 20, 70, 35, Math.PI / 5.4);
-  RocketTail(Canvas, 315, 435, 100, 150, 100, Math.PI / 4, '#D3D3D3', '#000000', 5, 20);
+  RocketTail(Canvas, 315, 435, 100, 150, 100, Math.PI / 4, "#D3D3D3", "#000000", 5, 20);
 
   Canvas.save();
 
@@ -71,12 +70,9 @@ function RocketEnd(Canvas, x, y, width, height, curveAmount, rotationAngle) {
   Canvas.fill();
 
   Canvas.restore();
-  
 
   RocketWings(Canvas, 330, 370, 20, 70, 35, Math.PI / 4);
-HandMid(Canvas, 397, 420, 100, 150, 100, Math.PI / 0.95, '#e69278');
-
-  
+  HandMid(Canvas, 397, 420, 100, 150, 100, Math.PI / 0.95, "#e69278");
 }
 
 function Circle(Canvas, x, y, radius, innerColor, borderColor, borderWidth) {
@@ -132,7 +128,14 @@ function Planet(Canvas, x, y, radius, color1, color2, rotationAngle) {
   Canvas.lineWidth = 10;
   Canvas.beginPath();
   Canvas.moveTo(x + radius, y);
-  Canvas.bezierCurveTo(x + radius + 70, y - radius / 4, x - radius - 70, y - radius / 4, x - radius, y);
+  Canvas.bezierCurveTo(
+    x + radius + 70,
+    y - radius / 4,
+    x - radius - 70,
+    y - radius / 4,
+    x - radius,
+    y
+  );
   Canvas.stroke();
 
   Canvas.restore();
@@ -176,70 +179,88 @@ function LightBulb(Canvas, x, y, radius) {
   Canvas.fill();
 }
 
-function RocketTail(Canvas, x, y, topWidth, bottomWidth, height, rotationAngle, fillColor, lineColor, lineWidth, lineOffset) {
-  
+function RocketTail(
+  Canvas,
+  x,
+  y,
+  topWidth,
+  bottomWidth,
+  height,
+  rotationAngle,
+  fillColor,
+  lineColor,
+  lineWidth,
+  lineOffset
+) {
   Canvas.save();
 
-
   Canvas.translate(x, y);
- 
+
   Canvas.rotate(rotationAngle);
-  
+
   Canvas.translate(-x, -y);
 
   const halfTopWidth = topWidth / 6;
   const halfBottomWidth = bottomWidth / 4.5;
   const topY = y - height / 4;
-  const bottomY = y + height / 4 ;
-  const curveAmount = 10; 
+  const bottomY = y + height / 4;
+  const curveAmount = 10;
 
- 
   Canvas.fillStyle = fillColor;
   Canvas.beginPath();
-  Canvas.moveTo(x - halfTopWidth, topY); 
+  Canvas.moveTo(x - halfTopWidth, topY);
 
   Canvas.bezierCurveTo(
-      x - halfTopWidth + curveAmount, topY - curveAmount, 
-      x + halfTopWidth - curveAmount, topY - curveAmount, 
-      x + halfTopWidth, topY
+    x - halfTopWidth + curveAmount,
+    topY - curveAmount,
+    x + halfTopWidth - curveAmount,
+    topY - curveAmount,
+    x + halfTopWidth,
+    topY
   );
 
   Canvas.bezierCurveTo(
-      x + halfTopWidth + curveAmount, topY + curveAmount,
-      x + halfBottomWidth + curveAmount, bottomY - curveAmount,
-      x + halfBottomWidth, bottomY
+    x + halfTopWidth + curveAmount,
+    topY + curveAmount,
+    x + halfBottomWidth + curveAmount,
+    bottomY - curveAmount,
+    x + halfBottomWidth,
+    bottomY
   );
 
-  
   Canvas.bezierCurveTo(
-      x + halfBottomWidth - curveAmount, bottomY + curveAmount,
-      x - halfBottomWidth + curveAmount, bottomY + curveAmount,
-      x - halfBottomWidth, bottomY
+    x + halfBottomWidth - curveAmount,
+    bottomY + curveAmount,
+    x - halfBottomWidth + curveAmount,
+    bottomY + curveAmount,
+    x - halfBottomWidth,
+    bottomY
   );
 
-  
   Canvas.bezierCurveTo(
-      x - halfBottomWidth - curveAmount, bottomY - curveAmount,
-      x - halfTopWidth - curveAmount, topY + curveAmount,
-      x - halfTopWidth, topY
+    x - halfBottomWidth - curveAmount,
+    bottomY - curveAmount,
+    x - halfTopWidth - curveAmount,
+    topY + curveAmount,
+    x - halfTopWidth,
+    topY
   );
 
   Canvas.closePath();
   Canvas.fill();
 
   Canvas.strokeStyle = lineColor;
-  Canvas.lineWidth = lineWidth-2;
+  Canvas.lineWidth = lineWidth - 2;
 
   const numLines = 3;
-  const spacing = (bottomWidth - topWidth) / (numLines + 1) ;
+  const spacing = (bottomWidth - topWidth) / (numLines + 1);
   for (let i = 1; i <= numLines; i++) {
-      const offsetX = halfTopWidth + i * spacing - (spacing * (numLines + 1) / 2) - lineOffset;
-      Canvas.beginPath();
-      Canvas.moveTo(x + offsetX, topY);
-      Canvas.lineTo(x + offsetX + (bottomWidth - topWidth) * (i - 1) / (numLines + 1), bottomY);
-      Canvas.stroke();
+    const offsetX = halfTopWidth + i * spacing - (spacing * (numLines + 1)) / 2 - lineOffset;
+    Canvas.beginPath();
+    Canvas.moveTo(x + offsetX, topY);
+    Canvas.lineTo(x + offsetX + ((bottomWidth - topWidth) * (i - 1)) / (numLines + 1), bottomY);
+    Canvas.stroke();
   }
-
 
   Canvas.restore();
 }
@@ -261,40 +282,52 @@ function HandMid(Canvas, x, y, topWidth, bottomWidth, height, rotationAngle, fil
   Canvas.moveTo(x - halfTopWidth, topY);
 
   Canvas.bezierCurveTo(
-    x - halfTopWidth + curveAmount, topY - curveAmount,
-    x + halfTopWidth - curveAmount, topY - curveAmount,
-    x + halfTopWidth, topY
+    x - halfTopWidth + curveAmount,
+    topY - curveAmount,
+    x + halfTopWidth - curveAmount,
+    topY - curveAmount,
+    x + halfTopWidth,
+    topY
   );
 
   Canvas.bezierCurveTo(
-    x + halfTopWidth + curveAmount, topY + curveAmount,
-    x + halfBottomWidth + curveAmount, bottomY - curveAmount,
-    x + halfBottomWidth, bottomY
+    x + halfTopWidth + curveAmount,
+    topY + curveAmount,
+    x + halfBottomWidth + curveAmount,
+    bottomY - curveAmount,
+    x + halfBottomWidth,
+    bottomY
   );
 
   Canvas.bezierCurveTo(
-    x + halfBottomWidth - curveAmount, bottomY + curveAmount,
-    x - halfBottomWidth + curveAmount, bottomY + curveAmount,
-    x - halfBottomWidth, bottomY
+    x + halfBottomWidth - curveAmount,
+    bottomY + curveAmount,
+    x - halfBottomWidth + curveAmount,
+    bottomY + curveAmount,
+    x - halfBottomWidth,
+    bottomY
   );
 
   Canvas.bezierCurveTo(
-    x - halfBottomWidth - curveAmount, bottomY - curveAmount,
-    x - halfTopWidth - curveAmount, topY + curveAmount,
-    x - halfTopWidth, topY
+    x - halfBottomWidth - curveAmount,
+    bottomY - curveAmount,
+    x - halfTopWidth - curveAmount,
+    topY + curveAmount,
+    x - halfTopWidth,
+    topY
   );
 
   Canvas.closePath();
   Canvas.fill();
 
-  Canvas.strokeStyle = '#bf7966';
+  Canvas.strokeStyle = "#bf7966";
   Canvas.lineWidth = 3;
-  // Draw additional line within the shape's boundaries
+
   const lineStartX = x - halfBottomWidth;
   const lineStartY = y + 15;
   const lineEndX = x + halfBottomWidth;
   const lineEndY = y + 15;
-  
+
   Canvas.beginPath();
   Canvas.moveTo(lineStartX, lineStartY);
   Canvas.lineTo(lineEndX, lineEndY);
@@ -303,69 +336,55 @@ function HandMid(Canvas, x, y, topWidth, bottomWidth, height, rotationAngle, fil
   Canvas.restore();
 }
 
-
 function Finger(Canvas, x, y, width, height, curveAmount, rotationAngle) {
-  
   for (var i = 0; i < 4; i++) {
-  Canvas.save();
+    Canvas.save();
 
     if (i === 0) {
       Canvas.translate(x + width / 2, y + height / 1.6);
       Canvas.rotate(rotationAngle);
-    }
-    else if (i === 1) {
+    } else if (i === 1) {
       Canvas.translate(x + width / 1.3, y + height / 2);
-      Canvas.rotate(rotationAngle+0.27);
-
-    }
-    else if (i === 2) {
+      Canvas.rotate(rotationAngle + 0.27);
+    } else if (i === 2) {
       Canvas.translate(x + width / 0.95, y + height / 2);
-      Canvas.rotate(rotationAngle+0.56);
-
-    }
-    else if (i === 3) {
+      Canvas.rotate(rotationAngle + 0.56);
+    } else if (i === 3) {
       Canvas.translate(x + width / 0.8, y + height / 1.6);
-      Canvas.rotate(rotationAngle+0.8);
-
+      Canvas.rotate(rotationAngle + 0.8);
     }
-    
-    
+
     Canvas.translate(-(x + width / 2), -(y + height / 2));
 
     Canvas.fillStyle = "#e69278";
     Canvas.beginPath();
 
-    
     Canvas.moveTo(x + width / 2, y);
 
-    
     Canvas.bezierCurveTo(
-      x + width * 0.1, 
-      y + curveAmount / 2, 
-      x + width * 0.8, 
-      y + height * 0.8, 
-      x + width, 
-      y + height 
+      x + width * 0.1,
+      y + curveAmount / 2,
+      x + width * 0.8,
+      y + height * 0.8,
+      x + width,
+      y + height
     );
 
-  
     Canvas.bezierCurveTo(
-      x + width * 0.2, 
-      y + height + curveAmount / 2, 
-      x + width * 0.05, 
-      y + height * 0.2, 
-      x + width / 2, 
-      y 
+      x + width * 0.2,
+      y + height + curveAmount / 2,
+      x + width * 0.05,
+      y + height * 0.2,
+      x + width / 2,
+      y
     );
 
     Canvas.closePath();
     Canvas.fill();
-    
-    
+
     Canvas.restore();
   }
-HandBottom(Canvas, 380, 420, 30, 70, 35);
-
+  HandBottom(Canvas, 380, 420, 30, 70, 35);
 }
 
 function HandBottom(Canvas, x, y, width, height, curveAmount) {
@@ -374,52 +393,41 @@ function HandBottom(Canvas, x, y, width, height, curveAmount) {
   Canvas.fillStyle = "#e69278";
   Canvas.beginPath();
 
-  
   Canvas.moveTo(x, y);
 
-  
   Canvas.lineTo(x + width, y);
 
-  
   Canvas.bezierCurveTo(
-    x + width + curveAmount / 2, y, 
-    x + width, y + curveAmount * 0.4, 
-    x + width, y + curveAmount 
+    x + width + curveAmount / 2,
+    y,
+    x + width,
+    y + curveAmount * 0.4,
+    x + width,
+    y + curveAmount
   );
 
-  
   Canvas.lineTo(x + width, y + height - curveAmount);
 
- 
   Canvas.bezierCurveTo(
-    x + width, y + height - curveAmount * 0.4, 
-    x + width + curveAmount / 2, y + height, 
-    x + width, y + height 
+    x + width,
+    y + height - curveAmount * 0.4,
+    x + width + curveAmount / 2,
+    y + height,
+    x + width,
+    y + height
   );
 
-  
   Canvas.lineTo(x, y + height);
 
-  
   Canvas.lineTo(x, y + curveAmount);
 
-  
-  Canvas.bezierCurveTo(
-    x, y + curveAmount * 0.4, 
-    x - curveAmount / 2, y, 
-    x, y 
-  );
+  Canvas.bezierCurveTo(x, y + curveAmount * 0.4, x - curveAmount / 2, y, x, y);
 
   Canvas.closePath();
   Canvas.fill();
 
   Canvas.restore();
 }
-
-
-
-
-
 
 RocketHead(Canvas, 350, 250, 100, 200, 35, Math.PI / 4);
 RocketMid(Canvas, 350, 250, 100, 200, 35, Math.PI / 4);
